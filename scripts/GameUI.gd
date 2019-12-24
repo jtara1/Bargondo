b723dbac41887fb8ee4f534a5bda7683b31bfce9
James T <github.jtara1@gmail.com>
2019-12-24 14:39:37 -0800

    feat: countdown timer in GameUI

--- scripts/GameUI.gd
+++ scripts/GameUI.gd
@@ -1,9 +1,12 @@
 extends Control
 
+signal game_timer_ended
+
 var pizza_count = 0
 
 onready var character = $"../../"
-onready var text_label = $NinePatchRect/Panel/RichTextLabel
+onready var text_label = $NinePatchRect/PizzaPanel/RichTextLabel
+onready var timer_text_label = $NinePatchRect/TimerPanel/TimerTextLabel
 
 func _ready():
 	character.connect("item_consumed", self, "_on_Character_item_consumed")
@@ -14,3 +17,13 @@ func change_pizza_count(amount):
 
 func _on_Character_item_consumed(item):
 	if item is Pizza: change_pizza_count(1)
+
+func _on_Timer_timeout():
+	var text = timer_text_label.text
+	var time_left = int(text)
+	
+	if time_left <= 0:
+		emit_signal("game_timer_ended")
+		timer_text_label.get_node("Timer").queue_free()
+	else:
+		timer_text_label.text = str(time_left - 1)
