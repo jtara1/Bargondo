faf398792b0ced9f3ebe6eb8eafb9026b60f4ae9
James T <github.jtara1@gmail.com>
2019-12-24 12:08:50 -0800

    feat(Enemy): add init wait time (option & exported var)

--- scripts/enemy/Enemy.gd
+++ scripts/enemy/Enemy.gd
@@ -1,3 +1,4 @@
 extends Node2D
 
 export(float) var speed = 1
+export(float) var init_delay_seconds = 0
--- scripts/enemy/EnemyMovement.gd
+++ scripts/enemy/EnemyMovement.gd
@@ -3,12 +3,19 @@ extends Node2D
 export(float) var close_enough_to_target_threshold = 25
 
 var target_index = 0
+var moveable = false
 
 onready var parent = $"../"
 onready var speed = parent.speed
 onready var movement_targets = [parent.position]
 
 func _ready():
+	var timer = Timer.new()
+	timer.wait_time = parent.init_delay_seconds
+	timer.one_shot = true
+	timer.connect("timeout", self, "_on_InitWaitTimer_timeout")
+	timer.start()
+	
 	load_movement_targets()
 
 func _physics_process(delta):
@@ -41,3 +48,6 @@ func load_movement_targets():
 		
 	for child in targets_container.get_children():
 		movement_targets.append(child.global_position)
+
+func _on_InitWaitTimer_timeout():
+	moveable = true
