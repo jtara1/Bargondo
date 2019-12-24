12675cfe9ecc6237c718a4c1a138a0d65fbf3c64
James T <github.jtara1@gmail.com>
2019-12-24 15:01:42 -0800

    feat(GameStateUI): finish game state ui, anim, timer & add usage in lvl0

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -4,12 +4,17 @@ var Explosion = load("res://game-objects/Explosion.tscn")
 
 onready var level_node = $"../"
 onready var character = $"../Character"
+onready var canvas = $"../CanvasLayer/GameStateUI"
 
 func _ready():
 	listen_to_character_death(character)
+	listen_to_game_countdown_timer(character)
 
 func listen_to_character_death(character):
 	character.connect("died", self, "_on_Character_died")
+
+func listen_to_game_countdown_timer(character):
+	character.get_node("CanvasLayer/GameUI").connect("game_timer_ended", self, "_on_GameUI_game_timer_ended")
 
 func reload_level():
 	get_tree().reload_current_scene()
@@ -25,4 +30,10 @@ func _on_Character_died(character):
 	anim_player.connect("animation_finished", self, "_on_Explosion_animation_finished")
 
 func _on_Explosion_animation_finished(anim_name):
+	reload_level()
+
+func _on_GameUI_game_timer_ended():
+	canvas.get_node("AnimationPlayer").play("game_state_ui_banner")
+
+func _on_AnimationPlayer_animation_finished(anim_name):
 	reload_level()
