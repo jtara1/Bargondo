467b0a46086d4d8075d1e787e5e11fd4646336cb
James T <github.jtara1@gmail.com>
2019-12-24 11:32:48 -0800

    feat: just reload lvl after death

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -2,18 +2,38 @@ extends Node2D
 
 var Explosion = load("res://game-objects/Explosion.tscn")
 var Character = load("res://game-objects/Character.tscn")
+var Pizza = load("res://game-objects/Pizza.tscn")
+
 var explosion = null
+var pizza_positions = []
 
 onready var level_node = $"../"
 onready var character = $"../Character"
 onready var respawn_flag = $"../RespawnFlag"
+onready var pizzas = $"../Pizzas"
+onready var pizzas_children = pizzas.get_children()
 
 func _ready():
 	listen_to_character_death(character)
 	
+	for child in pizzas_children:
+		pizza_positions.append(child.global_position)
+	
 func listen_to_character_death(character):
 	character.connect("died", self, "_on_Character_died")
+
+func reload_level():
+	get_tree().reload_current_scene()
 	
+func respawn_missing_pizzas():
+	for index in range(len(pizzas_children)):
+		if not pizzas_children[index]:
+			var pizza = Pizza.instance()
+			pizza.global_position = pizza_positions[index]
+			
+			pizzas.add_child(pizza)
+			pizzas[index] = pizza
+
 func _on_Character_died(character):
 	var character_position = character.position
 	character.visible = false
@@ -36,3 +56,6 @@ func _on_Explosion_animation_finished(anim_name):
 	
 	listen_to_character_death(new_character)
 	self.character = new_character
+	
+	reload_level()
+	
