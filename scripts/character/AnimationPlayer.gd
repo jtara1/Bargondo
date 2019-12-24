abe9bb552275d9136c22049871600ef2c08e753d
James T <github.jtara1@gmail.com>
2019-12-24 11:56:58 -0800

    fix(Char): reset sprite when anim starts

--- scripts/character/AnimationPlayer.gd
+++ scripts/character/AnimationPlayer.gd
@@ -13,11 +13,17 @@ func _ready():
 func play(name="", custom_blend=-1, custom_speed=1.0, from_end=false):
 	if is_in_camo: name = "camo_walk"
 	.play(name, custom_blend, custom_speed, from_end)
+
+func reset_sprite_transform():
+	sprite.position = Vector2()
+	sprite.rotation_degrees = 0
 	
 func _on_Character_camo_changed(camo):
 	is_in_camo = camo.type != "none"
 	sprite.frame = camo.parameters["sprite_frame"]
 	
 	if not is_in_camo:
-		sprite.position = Vector2()
-		sprite.rotation_degrees = 0
+		reset_sprite_transform()
+
+func _on_AnimationPlayer_animation_started(anim_name):
+	if anim_name != "camo_walk": reset_sprite_transform()
