07cce576e7d82815f0e041581366e28244da9ec6
James T <github.jtara1@gmail.com>
2019-12-23 17:05:30 -0800

    feat: adding on death

--- scripts/GameManager.gd
+++ scripts/GameManager.gd
@@ -1,5 +1,9 @@
 extends Node2D
 
+var Explosion = load("res://game-objects/Explosion.tscn")
+var Character = load("res://game-objects/Character.tscn")
+var explosion = null
+
 onready var level_node = $"../"
 onready var character = $"../Character"
 onready var respawn_flag = $"../RespawnFlag"
@@ -8,8 +12,25 @@ func _ready():
 	character.connect("died", self, "_on_Character_died")
 	
 func _on_Character_died(character):
-	var position = character.position
+	var character_position = character.position
 	character.queue_free()
 	
-	var sprite = Sprite.new()
+	var explosion = Explosion.instance()
+	explosion.position = character_position
+	level_node.add_child(explosion)
+	
+	var anim_player = explosion.get_node("AnimationPlayer")
+	anim_player.connect("animation_finished", self, "_on_Explosion_animation_finished")
+	anim_player.play("explosion")
+	return
+	
+func _on_Explosion_animation_finished(anim_name):
+	print('expl done')
+	if explosion: explosion.queue_free()
+	
+	var character = Character.instance()
+	character.position = respawn_flag.position
+	
+	level_node.add_child(character)
+	self.character = character
 	
--- scripts/character/Character.gd
+++ scripts/character/Character.gd
@@ -16,16 +16,14 @@ func consume(item):
 	item.queue_free()
 	
 func get_speed():
-#	return speed * $CharacterCamo.camo.get_speed_mult()
-	var spd = speed * $CharacterCamo.camo.get_speed_mult()
-	print(spd)
-	return spd
+	return speed * $CharacterCamo.camo.get_speed_mult()
 
 func process_collisions():
 	for index in get_slide_count():
 		var collision = get_slide_collision(index)
+
 		if "Enemy" in collision.collider.name:
 			die()
 
 func die():
-	emit_signal("died")
+	emit_signal("died", self)
